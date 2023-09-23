d4549d76b6da1763efec5b1269e6b386dc1c385a
ralfzzz <azbimanyu@gmail.com>
2023-09-23 08:32:56 +0700

    feat(install @tcgdex/sdk)

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from "express";
-import axios from "axios";
+import bodyParser from "body-parser";
+import TCGdex from '@tcgdex/sdk';
 
+const tcgdex = new TCGdex();
 const app = express();
 const port = 8005;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+const cards = await tcgdex.fetch("cards");
 
 app.use('/', (req,res) => {
-    res.render('index.ejs');
+    // console.log(cards);
+    res.render('index.ejs', {
+        inputs: cards
+    });
 })
+
+app.use('/get_card', async (req,res) => {
+
+
+})
+
+const get_card = await tcgdex.fetch('cards', 'card-global-id');
 
 app.listen(port, () => {
     console.log(`pokemon-card running on ${port}`);
