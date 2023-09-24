0ec8062b325014241a529111a432d39e96753c55
ralfzzz <azbimanyu@gmail.com>
2023-09-24 13:39:30 +0700

    mvp

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ import TCGdex from '@tcgdex/sdk';
 const tcgdex = new TCGdex();
 const app = express();
 const port = 8005;
+let card = '';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
-const cards = await tcgdex.fetch("cards");
 
-app.use('/', (req,res) => {
-    console.log(cards);
+app.get('/', async (req,res,next) => {
+    const cards = await tcgdex.fetch("cards");
     res.render('index.ejs', {
-        inputs: cards
+        inputs: cards,
+        card: card
     });
+    next();
 })
 
-app.use('/search', async (req,res) => {
-
-
+app.post('/search', async (req,res,next) => {
+    let globalId = req.body.search;
+    card = await tcgdex.fetch('cards', globalId);
+    res.redirect('/');
+    next();
 })
-
-const get_card = await tcgdex.fetch('cards', 'card-global-id');
 
 app.listen(port, () => {
     console.log(`pokemon-card running on ${port}`);
